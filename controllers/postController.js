a5f4b31b3f32206623b30d85b404691e00d49a19
ELHABACHI YOUSSEF <elhabachi.youssef@ofppt-edu.ma>
2024-03-01 15:23:34 +0100

    fix some code errors

--- controllers/postController.js
+++ controllers/postController.js
@@ -9,26 +9,27 @@ async function getUser(email) {
 }
 async function allPosts(req, res) {
   const posts = await Post.find();
-
+const user = await getUser(req.userEmail);
   let output = "<h1>ALL POSTS</h1>\n";
   posts.forEach((post) => {
-    output += `<p> <h3> ${post.title} </h3> <strong> content of post :   </strong>${post.content}</p> \n <strong>PostId :</strong>${post._id} <hr>`;
+    output += `<p> <h3> ${post.title} </h3> <strong> content of post :   </strong>${post.content}</p> \n <strong>User :</strong>${user.name} <hr>`;
   });
 
   return res.send(output);
 }
 async function show(req, res) {
   const user = await getUser(req.userEmail);
-  if (user.role == "admin") {
-  }
+
   Post.find({ userId: user._id })
     .then((posts) => {
       if (posts.length <= 0) {
         return res.status(404).send("No posts posted yet!!.");
       }
       let output = "<h1>YOUR POSTS</h1>\n";
+   
       posts.forEach((post, indx) => {
-        output += `<p> <h3> ${post.title} </h3> <strong> content of post :   </strong>${post.content}</p> \n <strong>PostId :</strong>${post._id} <hr>`;
+
+        output += `<p> <h3> ${post.title} </h3> <strong> content of post :   </strong>${post.content}</p> \n <strong>User :</strong>${user.email} <hr>`;
       });
       res.send(output);
     })
@@ -70,7 +71,9 @@ async function update(req, res) {
   const { id } = req.params;
   const { title, content, role } = req.body;
   const user = await getUser(req.userEmail);
-  if (user.role !== "admin") {
+
+  if (user.role == "admin") {
+  
     Post.findOneAndUpdate(
       { _id: id },
       {
@@ -88,6 +91,7 @@ async function update(req, res) {
         return res.status(400).send("Cannot Update this  post!");
       });
   } else {
+
     Post.findOneAndUpdate(
       { _id: id, userId: user._id },
       {
@@ -101,16 +105,17 @@ async function update(req, res) {
         return res.send("post Updated! " + post._id);
       })
       .catch((err) => {
-        console.log(err);
+       
         return res.status(400).send("Cannot Update this  post!");
       });
   }
+
 }
 
 async function remove(req, res) {
   const { id } = req.params;
   const user = await getUser(req.userEmail);
-  if (user.role !== "admin") {
+  if (user.role == "admin") {
     Post.findOneAndDelete(
       { _id: id },
       {
