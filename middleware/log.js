d129eaa9022e5e5ddac578740df92bc640e1742c
ELHABACHI YOUSSEF <elhabachi.youssef@ofppt-edu.ma>
2024-03-07 12:28:00 +0100

    remove serv.rest

--- middleware/log.js
+++ middleware/log.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const secret_key = "enji122u3u31g12tf21f31";
 function log(req, res, next) {
